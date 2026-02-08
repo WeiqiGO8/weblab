44336cf0eb64127a267ae7282d78ceed842fdc5b
WeiqiGO8 <hannah.lingzhi@gmail.com>
2026-02-08 21:01:43 +0100

    lab 2 part 1 in progress

--- js/lab2.js
+++ js/lab2.js
@@ -1,9 +1,37 @@
 console.log("LAB 2 part 1 & 2");
 
-// ! Task 1.
-let p_per_query = document.querySelector("p");
-console.log(p_per_query.parentElement);
+// ! lab 2 part 1 task 1.
+let pTagFinder1 = document.querySelector("p");
+console.log("The p tags parent is: " + pTagFinder1.parentElement);
 
-// ! Task 2.
-let span_container = document.querySelector("span");
-console.log(span_container.parentElement);
+// let pTagFinder = document.getElementById("paragraph1");
+// console.log("the p tags parent is: " + pTagFinder.parentElement);
+
+// ! lab 2 part 1 task 2.
+let span_container = document.getElementsByTagName("span");
+console.log(span_container);
+
+// ! Lab 2 part 1 task 3
+let listSibling = document.getElementById("list_2");
+console.log("second Sibling is: " + listSibling.previousElementSibling);
+console.log("first sibling is: " + listSibling.nextElementSibling);
+
+// ! Lab 2 part 1 task 4
+let childContainer = document.getElementById("childContainer");
+console.log(childContainer.childNodes);
+
+// ! Lab 2 part 1 task 5
+let paragraphElementContainer = document.getElementById(
+	"paragraphElementContainer",
+);
+
+console.log(
+	"firstElementChild: " + paragraphElementContainer.firstElementChild,
+);
+
+console.log("lastElementChild: " + paragraphElementContainer.lastElementChild);
+
+// ! Lab 2 part 1 task 6
+let multipleLevelsContainer = document.getElementById(
+	"multipleLevelsContainer",
+);
