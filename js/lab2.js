8fa125383201ec8d3c76c3b4004944bfc6aea1d2
WeiqiGO8 <hannah.lingzhi@gmail.com>
2026-02-09 19:23:09 +0100

    made progress on lab 2 part 1

--- js/lab2.js
+++ js/lab2.js
@@ -1,37 +1,47 @@
 console.log("LAB 2 part 1 & 2");
 
-// ! lab 2 part 1 task 1.
+//! lab 2 part 1 task 1.
 let pTagFinder1 = document.querySelector("p");
 console.log("The p tags parent is: " + pTagFinder1.parentElement);
 
 // let pTagFinder = document.getElementById("paragraph1");
 // console.log("the p tags parent is: " + pTagFinder.parentElement);
 
-// ! lab 2 part 1 task 2.
+//! lab 2 part 1 task 2.
 let span_container = document.getElementsByTagName("span");
 console.log(span_container);
 
-// ! Lab 2 part 1 task 3
+//! Lab 2 part 1 task 3
 let listSibling = document.getElementById("list_2");
 console.log("second Sibling is: " + listSibling.previousElementSibling);
 console.log("first sibling is: " + listSibling.nextElementSibling);
 
-// ! Lab 2 part 1 task 4
+//! Lab 2 part 1 task 4
 let childContainer = document.getElementById("childContainer");
 console.log(childContainer.childNodes);
 
-// ! Lab 2 part 1 task 5
-let paragraphElementContainer = document.getElementById(
-	"paragraphElementContainer",
+//! Lab 2 part 1 task 5
+let paragraphElementContainer = document.querySelector(
+	"#paragraphElementContainer",
 );
 
 console.log(
-	"firstElementChild: " + paragraphElementContainer.firstElementChild,
+	paragraphElementContainer.firstElementChild,
+	paragraphElementContainer.lastElementChild,
 );
 
-console.log("lastElementChild: " + paragraphElementContainer.lastElementChild);
-
-// ! Lab 2 part 1 task 6
-let multipleLevelsContainer = document.getElementById(
-	"multipleLevelsContainer",
+//! Lab 2 part 1 task 6
+let multipleLevelsContainer = document.querySelector(
+	"#multipleLevelsContainer",
 );
+
+console.log(multipleLevelsContainer.parentElement.parentElement);
+
+//! Lab 2 part 1 task 7
+let ul_7_1 = document.querySelector("#ul_7_1");
+let childrenOful_7_1 = ul_7_1.children;
+
+for (child of childrenOful_7_1) {
+}
+
+console.log("Children with ID number: ");
